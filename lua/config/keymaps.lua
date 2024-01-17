584160e8043e4e64d9e48db704bf633ddfa85912
Behnam Azizi <7786414+bazizi@users.noreply.github.com>
2024-01-17 00:29:13 -0800

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("v", "<leader>y", '"+y', { desc = "Copy to system clipboard" })
 vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>?", "I// <esc>j", { desc = "Comment out code line" })
+
